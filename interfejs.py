cd03fb0ebb48d4c2fe9f454fe319c62b27ad6c41
kaiokins <godfrydkuba@gmail.com>
2021-12-25 14:31:36 +0100

    Update interfejs.py

--- interfejs.py
+++ interfejs.py
@@ -1,5 +1,5 @@
 import biletomat
-from tkinter import Label, Tk, Button
+from tkinter import *
 from oprogramowanie import pieniadze as p
 from oprogramowanie import bilet as b
 from wyjatki import wyjatki as w
@@ -8,17 +8,48 @@ biletomat = biletomat.Biletomat()
 print(biletomat.suma())
 
 
-def action():
-    print("Siema")
+def action2():
+    print("Działa")
 
 
 root = Tk()
+root.title("Automat biletowy MPK")
 root.geometry("600x400")
 
-label = Label(root, text="Automat biletowy MPK", font=30)
+label = Label(root, text="Proszę wybrać rodzaj bilet", font=30)
 label.pack()
 
-test_button = Button(root, text="Klinij", width=8, command=action)
-test_button.pack()
+ulg20b = Button(root, text="20-minutowy ulgowy", command=action2)
+ulg20i = Entry(root, width=5)
+ulg20b.pack()
+ulg20i.pack()
+
+ulg40b = Button(root, text="40-minutowy ulgowy", command=action2)
+ulg40i = Entry(root, width=5)
+ulg40b.pack()
+ulg40i.pack()
+
+ulg60b = Button(root, text="60-minutowy ulgowy", command=action2)
+ulg60i = Entry(root, width=5)
+ulg60b.pack()
+ulg60i.pack()
+
+norm20b = Button(root, text="20-minutowy normalny", command=action2)
+norm20i = Entry(root, width=5)
+norm20b.pack()
+norm20i.pack()
+
+norm40b = Button(root, text="40-minutowy normalny", command=action2)
+norm40i = Entry(root, width=5)
+norm40b.pack()
+norm40i.pack()
+
+norm60b = Button(root, text="60-minutowy normalny", command=action2)
+norm60i = Entry(root, width=5)
+norm60b.pack()
+norm60i.pack()
+
+podsumowanie = Button(root, text="Podsumuj", command=action2)
+podsumowanie.pack()
 
 root.mainloop()
