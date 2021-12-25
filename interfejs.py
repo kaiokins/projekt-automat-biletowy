083d5d3622184aa8fdefa9a913b2a5c762f87601
kaiokins <godfrydkuba@gmail.com>
2021-12-25 14:00:04 +0100

    Testing interface

--- interfejs.py
+++ interfejs.py
@@ -1,7 +1,24 @@
 import biletomat
+from tkinter import Label, Tk, Button
 from oprogramowanie import pieniadze as p
 from oprogramowanie import bilet as b
 from wyjatki import wyjatki as w
 
 biletomat = biletomat.Biletomat()
 print(biletomat.suma())
+
+
+def action():
+    print("Siema")
+
+
+root = Tk()
+root.geometry("600x400")
+
+label = Label(root, text="Automat biletowy MPK", font=30)
+label.pack()
+
+test_button = Button(root, text="Klinij", width=8, command=action)
+test_button.pack()
+
+root.mainloop()
