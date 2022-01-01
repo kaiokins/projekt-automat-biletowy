f91d16103092ec314e1e3635fa61fa5abac6403b
kaiokins <godfrydkuba@gmail.com>
2022-01-01 21:26:21 +0100

    Dodatkowe wyjąki

--- wyjatki/wyjatki.py
+++ wyjatki/wyjatki.py
@@ -2,3 +2,6 @@ class bladUjemnaWartosc(Exception):
     def __init__(self, wiadomosc):
         self._wiadomosc = wiadomosc
 
+class bladZapisywaniaPliku(Exception):
+    def __init__(self, wiadomosc):
+        self._wiadomosc = wiadomosc
