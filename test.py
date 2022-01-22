1e25d531942f447be211b3ce27bb489014362572
kaiokins <godfrydkuba@gmail.com>
2022-01-22 11:33:23 +0100

    Poprawki

--- test.py
+++ test.py
@@ -8,24 +8,24 @@ class Testy(unittest.TestCase):
     def test_1(self):
         """Bilet kupiony za odliczoną kwotę - oczekiwany brak reszty"""
         root = Tk()
-        zl5i = Entry(root, width=5)
+        input = Entry(root, width=5)
         b = biletomat.Biletomat()
         bilet = []
         bilet.append(i.Bilety("20 minutowy", "ulgowy", Decimal('1.50'), 10))
-        b.dodajDoDepozytu('500', 1, zl5i)
-        b.dodajDoDepozytu('1000', 1, zl5i)
+        b.dodajDoDepozytu('500', 1, input)
+        b.dodajDoDepozytu('1000', 1, input)
         b.zaplac(i.zwrocCene(bilet))
         self.assertTrue(b.czyZostalaReszta() == 0)
 
     def test_2(self):
         """Bilet kupiony płacąc więcej - oczekiwana reszta"""
         root = Tk()
-        zl5i = Entry(root, width=5)
+        input = Entry(root, width=5)
         b = biletomat.Biletomat()
         bilet = []
         bilet.append(i.Bilety("20 minutowy", "ulgowy", Decimal('1.50'), 10))
-        b.dodajDoDepozytu('500', 1, zl5i)
-        b.dodajDoDepozytu('1000', 5, zl5i)
+        b.dodajDoDepozytu('500', 1, input)
+        b.dodajDoDepozytu('1000', 5, input)
         b.zaplac(i.zwrocCene(bilet))
         self.assertTrue(b.czyZostalaReszta() == 1)
 
@@ -36,13 +36,13 @@ class Testy(unittest.TestCase):
         for w interpreterze.
         """
         root = Tk()
-        zl5i = Entry(root, width=5)
+        input = Entry(root, width=5)
         b = biletomat.Biletomat()
         bilet = []
         bilet.append(i.Bilety("20 minutowy", "ulgowy", Decimal('1.50'), 1))
-        b.dodajDoDepozytu('1', 150, zl5i)
-        b.zaplac(i.zwrocCene(bilet))
+        b.dodajDoDepozytu('1', 150, input)
         self.assertEqual(i.zwrocCene(bilet), 1.50)
+        b.zaplac(i.zwrocCene(bilet))
         self.assertTrue(b.czyZostalaReszta() == 0)
 
     def test_5(self):
@@ -59,37 +59,34 @@ class Testy(unittest.TestCase):
         (wrzucone monety nie zerują się po dodaniu biletu).
         """
         root = Tk()
-        zl5i = Entry(root, width=5)
+        input = Entry(root, width=5)
         b = biletomat.Biletomat()
         bilet = []
-        bilet.append(i.Bilety("60 minutowy", "ulgowy", Decimal('1.50'), 10))
-        b.dodajDoDepozytu('500', 1, zl5i)
-        b.dodajDoDepozytu('1000', 1, zl5i)
-        bilet.append(i.Bilety("60 minutowy", "ulgowy", Decimal('1.50'), 1))
-        b.dodajDoDepozytu('100', 1, zl5i)
-        b.dodajDoDepozytu('50', 1, zl5i)
-        b.zaplac(i.zwrocCene(bilet))
+        bilet.append(i.Bilety("20 minutowy", "ulgowy", Decimal('1.50'), 10))
+        b.dodajDoDepozytu('500', 1, input)
+        b.dodajDoDepozytu('1000', 1, input)
+        bilet.append(i.Bilety("20 minutowy", "ulgowy", Decimal('1.50'), 1))
+        b.dodajDoDepozytu('100', 1, input)
+        b.dodajDoDepozytu('50', 1, input)
         self.assertEqual(i.zwrocCene(bilet), 16.50)
+        b.zaplac(i.zwrocCene(bilet))
         self.assertTrue(b.czyZostalaReszta() == 0)
 
     def test_7(self):
         "Próba wrzucenia ujemnej oraz niecałkowitej liczby monet (oczekiwany komunikat o błędzie)."
         root = Tk()
-        zl5i = Entry(root, width=5)
-        zl5i.insert(END, -3)
+        input = Entry(root, width=5)
+        input.insert(END, -3)
         blad_platnosci = Label(root, text="", font=30)
-        b = biletomat.Biletomat()
-        bilet = i.Bilety("60 minutowy", "ulgowy", Decimal('1.50'), 10)
-        self.assertTrue(i.sprawdzLiczbe(zl5i, blad_platnosci) == 0)
-        b.zaplac(bilet.zwrocCene())
+        self.assertTrue(i.sprawdzLiczbe(input, blad_platnosci) == 0)
 
     def test_dodatkowy(self):
         """Użytkownik wrzucił za małą ilość banknotów (oczekiwany komunikat o błędzie)"""
         root = Tk()
-        zl5i = Entry(root, width=5)
+        input = Entry(root, width=5)
         b = biletomat.Biletomat()
         bilet = i.Bilety("60 minutowy", "ulgowy", Decimal('3'), 1)
-        b.dodajDoDepozytu('1', 200, zl5i)
+        b.dodajDoDepozytu('1', 200, input)
         b.zaplac(bilet.zwrocCene())
         self.assertTrue(b.pobierzInformacje() == 3)
 
